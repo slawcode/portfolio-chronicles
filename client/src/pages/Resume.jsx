0781277aee6b2a88f17f97401e392ae61468293c
Sheryl L <s_lawrence0@yahoo.ca>
2024-06-09 16:09:03 -0400

    Button added to downloadable link of resume in Resume.jsx - still in progress

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -5,8 +5,12 @@ export default function Resume() {
         <div>
             <h1>My Resume</h1>
             <p>
-                Please view my resume to learn more about my previous work and education experiences!
+                Please view my resume to learn more about my previous work and educational experiences!
             </p>
+            <p>Hoover over the text below to reveal a button to download my resume:
+            </p>
+            <a href="." target='_blank' rel='noopener norefererrer' className='btn'>Download My Resume!</a>
         </div>
     )
 };
+
